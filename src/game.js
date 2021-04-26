9306206198639cfbd55250cafcd305d587700b88
Vladimircss <ermolovich@gmail.com>
2021-04-26 18:43:21 +0300

    debuging

--- src/game.js
+++ src/game.js
@@ -48,7 +48,7 @@ class Game extends React.Component {
         for(let i = 0; i < lines.length; i++){
             const [a, b, c] = lines[i];
             if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-                console.log(squares[a])
+                console.log(squares);
                 return squares[a];
             }
         }
@@ -78,6 +78,7 @@ class Game extends React.Component {
         const current =  history[this.state.stepNumber];
         const winner =  this.calculateWinner(current.squares);
         let status;
+        const drowCheck = current.squares.findIndex(el => el == null);
         const moves = history.map((step, move) => {
                 if(move > 0){
                     const desk = move > 1 ? `Перейти к ходу #${move - 1}` : "К началу игры";
@@ -109,7 +110,7 @@ class Game extends React.Component {
           <div className="game-info">
             <div>{status}</div>
             <ol>{moves}</ol>
-            {winner && (<button className='reset' onClick={() => this.resetGame()}>Начать заново</button>)}
+            {(drowCheck === -1 || winner) && (<button className='reset' onClick={() => this.resetGame()}>Начать заново</button>)}
           </div>
         </div>
       );
